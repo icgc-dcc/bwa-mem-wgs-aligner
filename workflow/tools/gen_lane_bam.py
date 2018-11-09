b95492aa09101fb9e3e8991edaa9371489bc142c
Junjun Zhang <junjun.ca@gmail.com>
2018-11-09 16:50:18 -0500

    try inline python code

---
+++ workflow/tools/gen_lane_bam.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+# just to enable testing for now
+with open("output.json", "w") as o:
+  o.write('{"bams": ["a.bam", "b.bam"]}')
---
+++ workflow/tools/pcawg_aligner.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+# just for testing for now
+with open("output.json", "w") as o:
+  o.write('{"merged_output_bam": "aligned.bam"}')
