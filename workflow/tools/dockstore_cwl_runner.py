9f8f9f40b2a4a2ae50d079555a84db014ec1a4d7
Junjun Zhang <junjun.ca@gmail.com>
2018-11-10 19:12:42 -0500

    added skeleton code for dockstore_cwl_runner

---
+++ workflow/tools/dockstore_cwl_runner.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import time
+
+"""
+This is fairly general approach, it could be a univeral tool that
+runs all PCAWG workflows defined in CWL
+
+1. download CWL workflow definition from Dockstore
+wget -O pcawg-bwa-mem.cwl pcawg_aligner_dockstore_url
+
+2. generate input yaml template
+cwltool --make-template pcawg-bwa-mem.cwl > input.yaml
+
+3. populate input.yaml with parameters provided from the arguments
+
+4. launch cwltool to run workflow
+
+5. report output
+"""
+
+
+time.sleep(20)
+
+
+with open("output.json", "w") as o:
+  o.write('{"merged_output_bam": "aligned.bam"}')
--- workflow/tools/pcawg_aligner.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import time
-
-# just to enable testing for now
-
-time.sleep(20)
-
-with open("output.json", "w") as o:
-  o.write('{"merged_output_bam": "aligned.bam"}')
