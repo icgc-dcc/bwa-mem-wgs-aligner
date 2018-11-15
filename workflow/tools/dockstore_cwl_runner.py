234e43f4108032670fa63b470d57539bc49ad78b
baminou <brice.aminou@gmail.com>
2018-11-15 15:49:52 -0500

    bwa-mem aligner added

--- workflow/tools/dockstore_cwl_runner.py
+++ workflow/tools/dockstore_cwl_runner.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 import time
+import requests
+import argparse
+import subprocess
+import yaml
+import json
 
 """
 This is fairly general approach, it could be a univeral tool that
@@ -18,9 +23,54 @@ cwltool --make-template pcawg-bwa-mem.cwl > input.yaml
 5. report output
 """
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--dockstore_tool_url', dest='dockstore_tool_url', nargs='+', help='CWL tool URL',default="https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/Seqware-BWA-Workflow/2.6.8_1.3/Dockstore.cwl")
+parser.add_argument('--output_dir', dest='output_dir', type=str, help='Output directory', required=True)
+parser.add_argument('--reads', dest='reads', nargs='+', help='List of reads', required=True)
+parser.add_argument('--output_file_basename', dest='output_file_basename', type=str, help='Output file basename', required=True)
+parser.add_argument('--download_reference_files', dest='download_reference_files', action='store_true', help='Download reference files', default=False)
+parser.add_argument('--reference_gz_amb', dest='reference_gz_amb', help='Reference genome amb', required=True)
+parser.add_argument('--reference_gz_sa', dest='reference_gz_sa', help='Reference genome sa', required=True)
+parser.add_argument('--reference_gz_pac', dest='reference_gz_pac', help='Reference genome pac', required=True)
+parser.add_argument('--reference_gz_ann', dest='reference_gz_ann', help='Reference genome ann', required=True)
+parser.add_argument('--reference_gz_bwt', dest='reference_gz_bwt', help='Reference genome bwt', required=True)
+parser.add_argument('--reference_gz_fai', dest='reference_gz_fai', help='Reference genome fai', required=True)
+parser.add_argument('--reference_gz', dest='reference_gz', help='Reference genome gz', required=True)
 
-time.sleep(20)
 
+
+
+args = parser.parse_args()
+
+
+with open('pcawg-bwa-mem.cwl','wb') as f:
+  f.write(requests.get(args.dockstore_tool_url).content)
+
+with open('input.yaml', 'w') as f:
+  subprocess.call(['cwltool','--make-template','pcawg-bwa-mem.cwl'], stdout=f)
+
+input_json = yaml.load(open('input.yaml'))
+input_json['reference_gz_sa']['path'] = args.reference_gz_sa
+input_json['reference_gz_pac']['path'] = args.reference_gz_pac
+input_json['reference_gz_fai']['path'] = args.reference_gz_fai
+input_json['reference_gz_bwt']['path'] = args.reference_gz_bwt
+input_json['reference_gz_ann']['path'] = args.reference_gz_ann
+input_json['reference_gz_amb']['path'] = args.reference_gz_amb
+input_json['reference_gz']['path'] = args.reference_gz
+input_json['reads'] = []
+
+for read in args.reads:
+  input_json['reads'].append({'class':'File','path':read})
+
+input_json['output_file_basename'] = args.output_file_basename
+input_json['output_dir'] = args.output_dir
+input_json['download_reference_files'] = str(args.download_reference_files).lower()
+
+input_json['reference_gz_sa']['path'] = args.reference_gz_sa
+with open('job.json', 'w') as fp:
+  json.dump(input_json,fp, indent=4, sort_keys=True)
+
+subprocess.call(['cwltool','pcawg-bwa-mem.cwl','job.json'])
 
 with open("output.json", "w") as o:
   o.write('{"merged_output_bam": "aligned.bam"}')
