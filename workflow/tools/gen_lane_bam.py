ecc87757df281fcb1cccf4dd741d0713414f0384
lindaxiang <linda.xiang@oicr.on.ca>
2018-11-20 16:21:47 -0500

    fix a typo

--- workflow/tools/gen_lane_bam.py
+++ workflow/tools/gen_lane_bam.py
@@ -113,18 +113,16 @@ for _file in files:
     file_name = _file.get('name')
     storage_site, analysis_id, object_id = file_path.strip('song://').split('/')
 
-    # output_dir = os.path.join(cwd, analysis_id)
-    # if not os.path.isdir(output_dir): os.makedirs(output_dir)
     try:
         subprocess.run(['score-client',
                                  '--profile', mapping.get(storage_site),
                                  'download',
                                  '--object-id', object_id,
                                  '--output-dir', cwd,
-                                 '--index', False,
+                                 '--index', 'false',
                                  '--force'], check=True)
     except Exception as e:
-        sys.exit('%s: Download object failed: %s' % (e, object_id))
+        sys.exit('\n%s: Download object failed: %s' % (e, object_id))
 
     # get all the rg for the _file
     rg_yaml = set()
@@ -147,7 +145,7 @@ for _file in files:
         header = subprocess.check_output(['samtools', 'view', '-H', os.path.join(cwd, file_name)])
 
     except Exception as e:
-        sys.exit('%s: Retrieve BAM header failed: %s' % (e, os.path.join(cwd, file_name)))
+        sys.exit('\n%s: Retrieve BAM header failed: %s' % (e, os.path.join(cwd, file_name)))
 
     # get @RG
     header_array = header.decode('utf-8').rstrip().split('\n')
@@ -160,7 +158,7 @@ for _file in files:
             rg_bam.add(':'.join(element.rstrip().split(':')[1:]))
 
     # compare the RG ids
-    if not rg_yaml == rg_bam: sys.exit('The read groups in metadata do not match with those in BAM!')  # die fast
+    if not rg_yaml == rg_bam: sys.exit('\nThe read groups in metadata do not match with those in BAM!')  # die fast
 
     # Revert the bam to unaligned and lane level bam sorted by query name
     output_dir = os.path.join(cwd, object_id, 'lane_unaligned')
@@ -170,7 +168,7 @@ for _file in files:
                         'RevertSam', 'I=%s' % os.path.join(cwd, file_name),
                         'OUTPUT_BY_READGROUP=true', 'O=%s' % output_dir], check=True)
     except Exception as e:
-        sys.exit('%s: RevertSam failed: %s' %(e, os.path.join(cwd, file_name)))
+        sys.exit('\n%s: RevertSam failed: %s' %(e, os.path.join(cwd, file_name)))
 
 
     # detect if read_group replacement are needed
@@ -184,7 +182,7 @@ for _file in files:
                                 'RGPU=%s' % rg_new.get('PU'), 'RGSM=%s' % rg_new.get('SM'), 'RGPM=%s' % rg_new.get('PM'),
                                 'RGCN=%s' % rg_new.get('CN'), 'RGPI=%s' % rg_new.get('PI'), 'RGDT=%s' % rg_new.get('DT')], check=True)
             except Exception as e:
-                sys.exit('%s: ReplaceReadGroups failed: %s' % (e, os.path.join(output_dir, rg_old+'.bam')))
+                sys.exit('\n%s: ReplaceReadGroups failed: %s' % (e, os.path.join(output_dir, rg_old+'.bam')))
 
 
     # no need to replace
@@ -203,7 +201,7 @@ for _file in files:
                             'C=library_strategy:%s' % metadata.get('library_strategy'),
                             'C=use_cntl:%s' % metadata.get('use_cntl', None)], check=True)
         except Exception as e:
-            sys.exit('%s: AddCommentsToBam failed: %s' %(e, os.path.join(output_dir, rg.get('read_group_id')+'.bam')))
+            sys.exit('\n%s: AddCommentsToBam failed: %s' %(e, os.path.join(output_dir, rg.get('read_group_id')+'.bam')))
 
         output_bams['bams'].append(os.path.join(output_dir, rg.get('read_group_id')+'.reheader.bam'))
 
