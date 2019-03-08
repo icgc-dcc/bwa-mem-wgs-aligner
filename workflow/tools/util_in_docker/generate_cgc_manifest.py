266e4dda3639113828f1e1f5c089de99d0b07703
baminou <brice.aminou@gmail.com>
2019-03-08 16:45:26 -0500

    bam and bai files uploaded together

--- workflow/tools/util_in_docker/generate_cgc_manifest.py
+++ workflow/tools/util_in_docker/generate_cgc_manifest.py
@@ -7,7 +7,7 @@ import json
 def main():
     """ Main program """
     parser = argparse.ArgumentParser(description='Convert SONG payload to CGC manifest file')
-    parser.add_argument('--filename',dest='filename', required=True)
+    parser.add_argument('--filenames',dest='filenames', nargs='+',required=True)
     parser.add_argument('--song-payload',dest='song_payload', type=argparse.FileType('r'), required=True)
     parser.add_argument('--output',dest='output', required=True)
 
@@ -16,15 +16,16 @@ def main():
     with open(results.output,'w') as output_fp:
         payload = json.load(results.song_payload)
         output_fp.write(','.join(['File name','experimental_strategy','case_id','aliquot_uuid','case_submitter_id','sample_class','study'])+'\n')
-        output_fp.write(','.join([
-            results.filename,
-            payload.get('experiment').get('libraryStrategy'),
-            payload.get('sample').get('donor').get('donorId'),
-            payload.get('sample').get('info').get('aliquotId'),
-            payload.get('sample').get('donor').get('donorSubmitterId'),
-            payload.get('sample').get('specimen').get('specimenClass'),
-            payload.get('study'),
-        ]))
+        for filename in results.filenames:
+            output_fp.write(','.join([
+                filename,
+                payload.get('experiment').get('libraryStrategy'),
+                payload.get('sample').get('donor').get('donorId'),
+                payload.get('sample').get('info').get('aliquotId'),
+                payload.get('sample').get('donor').get('donorSubmitterId'),
+                payload.get('sample').get('specimen').get('specimenClass'),
+                payload.get('study')
+            ])+'\n')
 
 if __name__ == "__main__":
     main()
